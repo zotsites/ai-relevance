79d95d686179f270d1c966711156aa00a7474d4c
daciwolf <davidculciar@gmail.com>
2024-06-20 00:32:16 -0700

    migration_brute attempt

--- database_migration.py
+++ database_migration.py
@@ -1,6 +1,9 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import psycopg2
+import json 
+from psycopg2.extras import RealDictCursor
 
 def read_password(filename):
     with open(filename, 'r') as f:
@@ -18,7 +21,19 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
+    posgress_database = psycopg2.connect(host='anthill.postgres.database.azure.com', dbname='zotsites', user='circinus', password='Admin123', port=5432)
+    postgres_cursor = posgress_database.cursor(cursor_factory=RealDictCursor)
+    query = "SELECT * FROM webpage;"
+    postgres_cursor.execute(query)
+    rows = postgres_cursor.fetchall()
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
+    db = client["cluster0"]
+    collection = db['websites_json']
+    print(type(rows[1]))
+    if isinstance(rows, list):
+        collection.insert_many(rows)
+
+
 except Exception as e:
     print(e)
